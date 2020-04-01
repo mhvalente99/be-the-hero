c36c5d2149e83f3fd24ebff1aa0399863dd92d8c
Matheus <matheushenrique4@hotmail.com>
2020-03-31 23:41:54 -0300

    add test

---
+++ README.md
@@ -0,0 +1,5 @@
+## To do 
+[ ] - create validation for incidents post
+[ ] - dark mode web
+[ ] - dark mode mobile
+[ ] - sender message with id for ONG by email and whatsapp
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -8,7 +8,6 @@ const app = express();
 app.use(cors()); //Configurar parametro origin para release
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(3333, () => {
-    console.log('Servidor rodando na porta 3333');
-});
+module.exports = app;
